884151bab5e207e3218a0cf56068682276f199bb
mutoe <mutoe@foxmail.com>
2020-09-28 00:20:41 +0800

    refactor: migrate to typescript

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,6 @@
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script lang="ts" setup>
+export { default as HelloWorld } from "./components/HelloWorld.vue";
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,15 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <h1>{{ props.msg }}</h1>
   <button @click="count++">count is: {{ count }}</button>
   <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      count: 0
-    }
-  }
+<script lang="ts" setup="props">
+import { ref } from "vue";
+
+declare const props: {
+  msg: string
 }
+
+export const count = ref(0)
 </script>
