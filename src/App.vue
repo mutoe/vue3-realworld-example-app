5ed2a51334f6deff4e1273d57723dbab82a7317c
Sándor Levcsák <levchak.sanek@gmail.com>
2020-10-02 01:15:56 +0300

    chore: add app navigation

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <RouterView></RouterView>
+  <AppNavigation />
+  <RouterView />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
+import AppNavigation from './components/layout/Navigation/index.vue'
+
 export default defineComponent({
   name: 'App',
+  components: {
+    AppNavigation
+  },
 })
 </script>
