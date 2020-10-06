fb14a4a9fc6c816597d6c4702bee08a5ddaeabe7
mutoe <mutoe@foxmail.com>
2020-10-06 20:05:41 +0800

    chore: add cypress example code

---
+++ cypress/integration/guest.spec.js
@@ -0,0 +1,13 @@
+describe('View the system by guest', () => {
+  it('should can access home page', () => {
+    cy.visit('/')
+
+    cy.get('h1.logo-font')
+      .should('contain.text', 'conduit')
+  })
+
+  it('should highlight Global Feed when home page loaded', () => {
+    cy.get('.nav').get('.nav-link.active')
+      .should('contain.text','Global Feed')
+  })
+})
