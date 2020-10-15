27535ca2f4f99e98af16deb5f36da48992cc4a7e
Sándor Levcsák <levchak.sanek@gmail.com>
2020-10-15 06:04:07 +0300

    feat: add utils for request and test

---
+++ src/utils/test/wrap-tests.ts
@@ -0,0 +1,23 @@
+interface WrapTestsProps <Item> {
+  task: string
+  list: Item[]
+  fn: (item: Item) => void,
+  only?: boolean,
+  testName?: (item: Item, index: number) => string
+}
+
+function wrapTests<Item> ({ task, list, fn, testName, only = false }: WrapTestsProps<Item>): void {
+  const descFn = only ? describe.only : describe
+
+  descFn(task, () => {
+    list.forEach((item, index) => {
+      const name = testName ? testName(item, index) : ''
+      it(name, () => fn(item))
+    })
+  })
+}
+wrapTests.only = function <Item> ({ task, list, fn, testName }: WrapTestsProps<Item>): ReturnType<typeof wrapTests> {
+  wrapTests({ task, list, fn, testName, only: true })
+}
+
+export default wrapTests
