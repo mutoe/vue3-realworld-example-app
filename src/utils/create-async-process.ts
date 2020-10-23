916291bf9d8ab6224095143b9700dd96b80ae2c1
Sándor Levcsák <levchak.sanek@gmail.com>
2020-10-23 06:41:11 +0300

    fix(util/create-async-process): pass arguments

--- src/utils/create-async-process.ts
+++ src/utils/create-async-process.ts
@@ -3,9 +3,9 @@ import { ref } from 'vue'
 export default function createAsyncProcess<T extends (...args: any) => Promise<any>> (fn: T) {
   const active = ref<boolean>(false)
 
-  async function run (): Promise<ReturnType<T>> {
+  async function run (...args : any): Promise<ReturnType<T>> {
     active.value = true
-    const result = await fn()
+    const result = await fn(...args)
     active.value = false
     return result
   }
