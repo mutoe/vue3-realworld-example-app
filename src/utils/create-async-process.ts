d5bf11a8cdebe321b0798fb3af99329af7606ac1
Sándor Levcsák <levchak.sanek@gmail.com>
2020-10-23 07:29:39 +0300

    fix(utils/create-async-process): improve types

--- src/utils/create-async-process.ts
+++ src/utils/create-async-process.ts
@@ -1,9 +1,9 @@
 import { ref } from 'vue'
 
-export default function createAsyncProcess<T extends (...args: any) => Promise<any>> (fn: T) {
+export default function createAsyncProcess<T extends (...args: any[]) => any> (fn: T) {
   const active = ref<boolean>(false)
 
-  async function run (...args : any): Promise<ReturnType<T>> {
+  async function run (...args : Parameters<T>): Promise<ReturnType<T>> {
     active.value = true
     const result = await fn(...args)
     active.value = false
