774b0a1eabf88e6817caa4c5fed9807f2e675026
mutoe <mutoe@foxmail.com>
2020-10-26 22:56:53 +0800

    test: add test example cases

--- cypress/integration/guest.spec.js
+++ cypress/integration/guest.spec.js
@@ -1,13 +1,27 @@
-describe('View the system by guest', () => {
-  it('should can access home page', () => {
-    cy.visit('/')
+describe('View the homepage by guest', () => {
+  beforeEach(() => {
+    cy.server()
+    cy.route2('GET', /articles/, { fixture: 'articles.json' }).as('getArticles')
+    cy.route2('GET', /tags/, { fixture: 'tags.json' })
 
+    cy.visit('/')
+    cy.wait('@getArticles')
+  })
+
+  it('should can access home page', () => {
     cy.get('h1.logo-font')
       .should('contain.text', 'conduit')
   })
 
   it('should highlight Global Feed when home page loaded', () => {
-    cy.get('.nav').get('.nav-link.active')
-      .should('contain.text', 'Global Feed')
+    cy.get('.feed-toggle > .nav')
+      .contains('Global Feed')
+      .should('have.class', 'active')
+  })
+
+  it('should display article when page loaded', () => {
+    cy.get('.article-preview:first')
+      .find('h1')
+      .should('contain.text', 'abc123')
   })
 })
