5aa0f3308b378e75f87e0301c42faa7cb2116e64
Sándor Levcsák <levchak.sanek@gmail.com>
2020-11-01 06:59:39 +0200

    test: add tests for new request methods

--- src/utils/request.spec.ts
+++ src/utils/request.spec.ts
@@ -1,8 +1,11 @@
 import FetchRequest from 'src/utils/request'
+import { Either, fail, isEither, success } from 'src/utils/either'
 
 import params2query from 'src/utils/params-to-query'
 import mockFetch from 'src/utils/test/mock-fetch'
 import wrapTests from 'src/utils/test/wrap-tests'
+
+import { NetworkError } from 'src/types/error'
 
 beforeEach(() => {
   mockFetch({ type: 'body' })
@@ -17,31 +20,59 @@ const SUB_PREFIX = '/sub-prefix'
 const PATH = '/path'
 const PARAMS = { q1: 'q1', q2: 'q2' }
 
-type SafeMethod = 'get' | 'delete'
-type UnsafeMethod = 'post' | 'put' | 'patch'
+type SafeMethod = 'get' | 'delete' | 'checkableGet' | 'checkableDelete'
+type UnsafeMethod = 'post' | 'put' | 'patch' | 'checkablePost' | 'checkablePut' | 'checkablePatch'
 type Method = SafeMethod | UnsafeMethod
 
-const SAFE_METHODS: SafeMethod[] = ['get', 'delete']
-const UNSAFE_METHODS: UnsafeMethod[] = ['post', 'put', 'patch']
+type CheckableSafeMethod = 'checkableGet' | 'checkableDelete'
+type CheckableUnsafeMethod = 'checkablePost' | 'checkablePut' | 'checkablePatch'
+type CheckableMethod = CheckableSafeMethod | CheckableUnsafeMethod
 
 function isSafe (method: Method): method is SafeMethod {
   return ['get', 'delete'].includes(method)
 }
-
-async function triggerMethod<T = any> (request: FetchRequest, method: Method, options?: any): Promise<T> {
-  let body: T
-  if (isSafe(method)) body = await request[method]<T>(PATH, options)
-  else body = await request[method]<T>(PATH, {}, options)
-  return body
+function isCheckableSafe (method: CheckableMethod): method is CheckableSafeMethod {
+  return ['checkableGet', 'checkableDelete'].includes(method)
+}
+function isCheckable (method: CheckableMethod | Method): method is CheckableMethod {
+  return ['checkableGet', 'checkableDelete', 'checkablePost', 'checkablePut', 'checkablePatch'].includes(method)
 }
 
-function forAllMethods (task: string, fn: (method: Method) => void): void {
+async function triggerMethod<T = any> (request: FetchRequest, method: Method | CheckableMethod, options?: any): Promise<T | Either<NetworkError, T>> {
+  if (isCheckable(method)) {
+    let response: Either<NetworkError, T>
+    if (isCheckableSafe(method)) response = await request[method]<T>(PATH, options)
+    else response = await request[method]<T>(PATH, {}, options)
+    return response.isOk() ? success(response.value) : fail(response.value)
+  } else {
+    let body: T
+    if (isSafe(method)) body = await request[method]<T>(PATH, options)
+    else body = await request[method]<T>(PATH, {}, options)
+    return body
+  }
+}
+
+function forCorrectMethods (task: string, fn: (method: Method) => void): void {
   wrapTests<Method>({
     task,
     fn,
-    list: [...UNSAFE_METHODS, ...SAFE_METHODS],
+    list: ['get', 'delete', 'post', 'put', 'patch'],
     testName: method => `for method: ${method}`,
   })
+}
+
+function forCheckableMethods (task: string, fn: (method: CheckableMethod) => void): void {
+  wrapTests<CheckableMethod>({
+    task,
+    fn,
+    list: ['checkableGet', 'checkableDelete', 'checkablePost', 'checkablePut', 'checkablePatch'],
+    testName: method => `for method: ${method}`,
+  })
+}
+
+function forAllMethods (task: string, fn: (method: Method | CheckableMethod) => void): void {
+  forCheckableMethods(task, fn)
+  forCorrectMethods(task, fn)
 }
 
 forAllMethods('# Should be implemented', async (method) => {
@@ -50,7 +81,7 @@ forAllMethods('# Should be implemented', async (method) => {
   triggerMethod(request, method)
 
   expect(global.fetch).toBeCalledWith(PATH, expect.objectContaining({
-    method: method.toUpperCase(),
+    method: method.replace('checkable', '').toUpperCase(),
   }))
 })
 
@@ -131,7 +162,7 @@ describe('# Should work with headers', function () {
   })
 })
 
-forAllMethods('# Should converted response body to json', async function (method) {
+forCorrectMethods('# Should converted correct response body to json', async function (method) {
   const DATA = { foo: 'bar' }
   mockFetch({ type: 'body', ...DATA })
   const request = new FetchRequest()
@@ -141,7 +172,24 @@ forAllMethods('# Should converted response body to json', async function (method
   expect(body).toMatchObject(DATA)
 })
 
-forAllMethods('# Should throw Error with response when request status code is not 2xx', async function (method) {
+forCheckableMethods('# Should converted checkable response to Either<NetworkError, DATA_TYPE>', async function (method) {
+  const DATA = { foo: 'bar' }
+  type DATA_TYPE = {foo: 'bar'}
+  mockFetch({ type: 'body', ...DATA })
+  const request = new FetchRequest()
+
+  const result = await triggerMethod<DATA_TYPE>(request, method)
+
+  const resultIsEither = isEither<NetworkError, DATA_TYPE>(result)
+  const resultIsOk = isEither<NetworkError, DATA_TYPE>(result) && result.isOk()
+  const resultValue = isEither<NetworkError, DATA_TYPE>(result) && result.isOk() ? result.value : null
+
+  expect(resultIsEither).toBe(true)
+  expect(resultIsOk).toBe(true)
+  expect(resultValue).toMatchObject(DATA)
+})
+
+forCorrectMethods('# Should throw NetworkError if correct request is not OK', async function (method) {
   mockFetch({
     type: 'full',
     ok: false,
@@ -149,9 +197,32 @@ forAllMethods('# Should throw Error with response when request status code is no
     statusText: 'Bad request',
     json: async () => ({}),
   })
-  const request = new FetchRequest()
 
-  await expect(triggerMethod(request, method)).rejects.toThrow('Bad request')
+  const request = new FetchRequest()
+  const result = triggerMethod(request, method)
+
+  await expect(result).rejects.toBeInstanceOf(NetworkError)
+})
+
+forCheckableMethods('# Should return Either<NetworkError, DATA_TYPE> if checkable request is not OK', async function (method) {
+  mockFetch({
+    type: 'full',
+    ok: false,
+    status: 400,
+    statusText: 'Bad request',
+    json: async () => ({}),
+  })
+
+  const request = new FetchRequest()
+  const result = await triggerMethod(request, method)
+
+  const resultIsEither = isEither<NetworkError, void>(result)
+  const resultIsNotOk = isEither<NetworkError, void>(result) && result.isFail()
+  const resultValue = isEither<NetworkError, void>(result) && result.isFail() ? result.value : null
+
+  expect(resultIsEither).toBe(true)
+  expect(resultIsNotOk).toBe(true)
+  expect(resultValue).toBeInstanceOf(NetworkError)
 })
 
 describe('# Authorization header', function () {
