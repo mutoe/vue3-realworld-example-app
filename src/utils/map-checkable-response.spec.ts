6ccc0cda66f99188522861fdb3a8f1c393ff8e9c
Sándor Levcsák <levchak.sanek@gmail.com>
2020-11-04 03:24:26 +0200

    test: add tests for map-checkable-response utils

---
+++ src/utils/map-checkable-response.spec.ts
@@ -0,0 +1,76 @@
+import { ValidationError, AuthorizationError, NetworkError } from 'src/types/error'
+import { Either, fail, isEither, success } from './either'
+import { mapAuthorizationResponse, mapValidationResponse } from './map-checkable-response'
+
+const createCheckableResponse = (response: Partial<Response>): Either<NetworkError, Partial<Response>> => response.ok
+  ? success(response)
+  : fail(new NetworkError(response as Response))
+
+describe('# mapAuthorizationResponse', function () {
+  it('should return Either with AuthorizationError and correct Response', function () {
+    const RESPONSE = { ok: true }
+    const response = createCheckableResponse(RESPONSE)
+
+    const result = mapAuthorizationResponse<Partial<Response>>(response)
+
+    expect(isEither(result)).toBe(true)
+    expect(result.isOk()).toBe(true)
+    expect(result.value).toEqual(RESPONSE)
+  })
+
+  it('should return Either with AuthorizationError and failed Response', function () {
+    const RESPONSE = { ok: false, status: 401 }
+    const response = createCheckableResponse(RESPONSE)
+
+    const result = mapAuthorizationResponse<Partial<Response>>(response)
+
+    expect(isEither(result)).toBe(true)
+    expect(result.isFail()).toBe(true)
+    expect(result.value).toBeInstanceOf(AuthorizationError)
+  })
+
+  it('should throw NetworkError when Response is failed with status != 401', function () {
+    const RESPONSE = { ok: false, status: 400 }
+    const response = createCheckableResponse(RESPONSE)
+
+    expect(() => {
+      mapAuthorizationResponse<Partial<Response>>(response)
+    }).toThrowError(NetworkError)
+  })
+})
+
+describe('# mapValidationResponse', function () {
+  interface ValidationErrors {[field: string]: string}
+
+  it('should return Either with ValidationError and correct Response', function () {
+    const RESPONSE = { ok: true }
+    const response = createCheckableResponse(RESPONSE)
+
+    const result = mapValidationResponse<ValidationErrors, Partial<Response>>(response)
+
+    expect(isEither(result)).toBe(true)
+    expect(result.isOk()).toBe(true)
+    expect(result.value).toEqual(RESPONSE)
+  })
+
+  it('should return Either with ValidationError and failed Response', async function () {
+    const RESPONSE = { ok: false, status: 422, json: () => Promise.resolve({ errors: { foo: 'bar' } }) }
+    const response = createCheckableResponse(RESPONSE)
+
+    const result = mapValidationResponse<ValidationErrors, Partial<Response>>(response)
+
+    expect(isEither(result)).toBe(true)
+    expect(result.isFail()).toBe(true)
+    expect(result.value).toBeInstanceOf(ValidationError)
+    expect(result.isFail() && await result.value.getErrors()).toEqual((await RESPONSE.json()).errors)
+  })
+
+  it('should throw NetworkError when Response is failed with status != 422', function () {
+    const RESPONSE = { ok: false, status: 400 }
+    const response = createCheckableResponse(RESPONSE)
+
+    expect(() => {
+      mapValidationResponse<ValidationErrors, Partial<Response>>(response)
+    }).toThrowError(NetworkError)
+  })
+})
