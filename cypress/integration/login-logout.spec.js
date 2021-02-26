acc6abfb540c77b153e9dd36d53dd85139b20ef4
mutoe <mutoe@foxmail.com>
2021-02-27 00:18:02 +0800

    fix: adaptive cypress 6 intercept method

--- cypress/integration/login-logout.spec.js
+++ cypress/integration/login-logout.spec.js
@@ -1,10 +1,9 @@
 describe('test for login', () => {
   beforeEach(() => {
-    cy.server()
-    cy.route2('GET', /articles\?tag=butt/, { fixture: 'article_of_tag' }).as('article_of_tag')
-    cy.route2('GET', /articles\?/, { fixture: 'articles.json' }).as('getArticles')
-    cy.route2('GET', /articles\//, { fixture: 'article.json' }).as('getArticle')
-    cy.route2('GET', /tags/, { fixture: 'tags.json' }).as('getTags')
+    cy.intercept('GET', /articles\?tag=butt/, { fixture: 'article_of_tag' }).as('article_of_tag')
+    cy.intercept('GET', /articles\?/, { fixture: 'articles.json' }).as('getArticles')
+    cy.intercept('GET', /articles\//, { fixture: 'article.json' }).as('getArticle')
+    cy.intercept('GET', /tags/, { fixture: 'tags.json' }).as('getTags')
 
     cy.visit('/')
   })
