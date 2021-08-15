e28ea2b23336b24c93031912b8c6021aff3b0184
mutoe <mutoe@foxmail.com>
2021-08-15 16:06:59 +0800

    test: supplement params2query test

---
+++ src/utils/params-to-query.spec.ts
@@ -0,0 +1,15 @@
+import { request } from '../services'
+import params2query from './params-to-query'
+
+describe('# params2query', () => {
+  it('should return query string given an object', () => {
+    const params = {
+      foo: 'bar',
+      foo2: 'bar2',
+    }
+
+    const result = params2query(params)
+
+    expect(result).toEqual('foo=bar&foo2=bar2')
+  })
+})
