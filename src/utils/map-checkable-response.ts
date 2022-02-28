fcf0771cf3cc29818d8a66e6dcb5c58ca6ff249b
Sajad Hashemian <sajadhsm@gmail.com>
2022-02-28 11:50:04 +0330

    fix: mapValidationResponse not handling 403 error

--- src/utils/map-checkable-response.ts
+++ src/utils/map-checkable-response.ts
@@ -15,7 +15,7 @@ export const mapAuthorizationResponse = <T>(result: Either<NetworkError, T>): Ei
 export const mapValidationResponse = <E, T>(result: Either<NetworkError, T>): Either<ValidationError<E>, T> => {
   if (result.isOk()) {
     return success(result.value)
-  } else if (result.value.response.status === 422) {
+  } else if ([422, 403].includes(result.value.response.status)) {
     return fail(new ValidationError<E>(result.value.response))
   } else {
     throw result.value
