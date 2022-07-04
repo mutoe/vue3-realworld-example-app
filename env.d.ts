2796ccb48726b2912d13c7cbda5c7836da5ff42a
zhang.kaiyu <zhang.kaiyu@immomo.com>
2022-07-04 10:32:27 +0800

    fix:vscode ts2307

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+/* eslint-disable */
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
