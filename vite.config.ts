9361ea9df55713230b9255bab9fbc8aaac31cae7
mutoe <imutoe@gmail.com>
2023-09-06 19:55:16 +0800

    chore: update vite project

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import analyzer from 'rollup-plugin-analyzer'
@@ -7,7 +7,7 @@ import analyzer from 'rollup-plugin-analyzer'
 export default defineConfig({
   resolve: {
     alias: {
-      src: fileURLToPath(new URL('./src', import.meta.url)),
+      src: fileURLToPath(new URL('src', import.meta.url)),
     },
   },
   plugins: [
