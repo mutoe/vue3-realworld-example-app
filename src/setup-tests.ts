75d949e5f94747d93ac8e44f73e60e3b7170dc36
mutoe <imutoe@gmail.com>
2025-09-25 18:33:27 +0800

    chore: upgrade dependencies

--- src/setup-tests.ts
+++ src/setup-tests.ts
@@ -1,4 +1,6 @@
 /// <reference types="vitest-dom/extend-expect" />
+
+// TODO: migrate to @testing-library/jest-dom/vitest
 import 'vitest-dom/extend-expect'
 
 // https://github.com/mswjs/msw/issues/1415#issuecomment-1650562700
